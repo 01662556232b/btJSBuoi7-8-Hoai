2cb12895fc1603e7bff888ebe94128f77f866083
unknown <01662556232b@gmail.com>
2022-07-25 20:13:25 +0700

    sua loi

--- js/main.js
+++ js/main.js
@@ -33,23 +33,23 @@ function demSoDuong() {
 }
 document.getElementById("btndemsoduong").onclick = demSoDuong;
 //bài tap 3
-function timmin() {
-    var vitri = 0;
-    var Min = soNguyen[0];
+function timMin() {
+    var viTri = 0;
+    var min = soNguyen[0];
     for (var i = 0; i < soNguyen.length; i++) {
-        if (Min > soNguyen[i]) {
+        if (min > soNguyen[i]) {
 
-            Min = soNguyen[i];
+            min = soNguyen[i];
             vitri = i;
 
         }
     }
-    document.querySelector("#txtResult3").innerHTML = "Số nhỏ nhất: " + Min;
+    document.querySelector("#txtResult3").innerHTML = "Số nhỏ nhất: " + min;
 }
-document.getElementById("btnsonhonhat").onclick = timmin;
+document.getElementById("btnsonhonhat").onclick = timMin;
 
 //bài 4
-function timsoduongnhonhat() {
+function timSoDuongNhoNhat() {
     var viTri = 0;
     var min = soNguyen[0];
     for (var i = 0; i < soNguyen.length; i++) {
@@ -62,9 +62,9 @@ function timsoduongnhonhat() {
     }
     document.querySelector("#txtResult4").innerHTML = "Số dương nhỏ nhất: " + min;
 }
-document.getElementById("btnduongnhonhat").onclick = timsoduongnhonhat;
+document.getElementById("btnduongnhonhat").onclick = timSoDuongNhoNhat;
 //bài 5
-function timsoduongcuoicung() {
+function timSoDuongCuoiCung() {
 
     var numberCuaSuTanCung = -1;
     for (var i = 0; i < soNguyen.length; i++) {
@@ -76,10 +76,10 @@ function timsoduongcuoicung() {
     }
     document.querySelector("#txtResult5").innerHTML = "Số chẵn cuối cùng: " + numberCuaSuTanCung;
 }
-document.getElementById("btnduongcuoicung").onclick = timsoduongcuoicung;
+document.getElementById("btnduongcuoicung").onclick = timSoDuongCuoiCung;
 
 //bài6
-function checkdoicho(a, b) {
+function checkDoiCho(a, b) {
     temp = soNguyen[a];
     soNguyen[a] = soNguyen[b];
     soNguyen[b] = temp;
@@ -89,18 +89,18 @@ function checkdoicho(a, b) {
 function doiCHo() {
     var inputSo1 = document.getElementById("so1").value;
     var inputSo2 = document.getElementById("so2").value;
-    checkdoicho(inputSo1, inputSo2);
+    checkDoiCho(inputSo1, inputSo2);
 
     document.querySelector('#txtResult6').innerHTML = "Mảng sau khi đổi: " + soNguyen;
 }
 //bài7
-function sapxep() {
+function sapXep() {
 
     for (var i = 0; i < soNguyen.length - 1; i++) {
         for (var j = 0; j < soNguyen.length; j++) {
 
             if (soNguyen[j] > soNguyen[j + 1]) {
-                checkdoicho(j, j + 1)
+                checkDoiCho(j, j + 1)
             }
 
         }
@@ -109,11 +109,11 @@ function sapxep() {
     console.log(soNguyen)
     document.querySelector('#txtResult7').innerHTML = "Mảng sau khi sắp xếp: " + soNguyen;
 }
-document.querySelector("#btnsapxep").onclick = sapxep;
+document.querySelector("#btnsapxep").onclick = sapXep;
 
 //bài8
 
-function checknt(num) {
+function checkNT(num) {
     for (var i = 2, s = Math.sqrt(num); i <= s; i++)
         if (num % i === 0) {
             return false;
@@ -123,7 +123,7 @@ function checknt(num) {
 function nguyenTo() {
     var nguyento = -1; // trả về -1 khi không có số nguyên tố
     for (var i = 0; i < soNguyen.length; i++) {
-        if (checknt(soNguyen[i])) {
+        if (checkNT(soNguyen[i])) {
             nguyento = soNguyen[i];
             break;
         }
@@ -134,22 +134,22 @@ document.querySelector("#btnnguyento").onclick = nguyenTo;
 
 //bai9
 var soNguyen2 = [];
-function themsothuc() {
+function themSoThuc() {
     var number = Number(document.getElementById("nhapso").value);
     soNguyen2.push(number);
     document.querySelector("#txtResult9").innerHTML = soNguyen2;
 }
-document.querySelector("#btsothuc").onclick = themsothuc;
-function demsonguyen() {
+document.querySelector("#btsothuc").onclick = themSoThuc;
+function demSoNguyen() {
     var count = 0;
     for (var i = 0; i < soNguyen2.length; i++) {
         Number.isInteger(soNguyen2[i]) ? count++ : count;
     }
     document.getElementById('txtResultdemsobai9').innerHTML = count;
 }
-document.querySelector("#btdemso").onclick = demsonguyen;
+document.querySelector("#btdemso").onclick = demSoNguyen;
 //bai10
-function sosanh() {
+function soSanh() {
     var countDuong = 0;
     var countAm = 0;
     ketQua = '';
@@ -165,4 +165,4 @@ function sosanh() {
   document.getElementById('txtResult10').innerHTML = ketQua
 
 }
-document.querySelector("#btsosanh").onclick = sosanh;
+document.querySelector("#btsosanh").onclick = soSanh;
